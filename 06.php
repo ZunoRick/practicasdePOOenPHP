71b7cc5c116d0bd3974c6cd7215d3a42801e5ac4
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-09 19:12:49 -0500

    Modifique el archivo 06-Interfaces

--- 06.php
+++ 06.php
@@ -1,4 +1,25 @@
 <?php include 'includes/header.php';
 
 
+//Interfaces: permiten agrupar un grupo de declaraciones de funciones pero sin implementar
+
+interface TransporteInterfaz{
+    public function GetInfo() : string;
+    public function GetRuedas() : int;
+}
+
+class Transporte implements TransporteInterfaz{
+    public function __construct(protected int $ruedas, protected int $capacidad) {
+        
+    }
+
+    public function GetInfo() : string {
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas.";
+    }
+
+    public function GetRuedas() : int {
+        return $this->ruedas;
+    }
+}
+
 include 'includes/footer.php';
