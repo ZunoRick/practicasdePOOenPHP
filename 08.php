2ba16809d82d80d658b55255510ee7e2377ae3f5
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-12 18:52:07 -0500

    Agregando dos nuevas clases y modificando el archivo 08-Autoload de Clases

--- 08.php
+++ 08.php
@@ -1,4 +1,17 @@
 <?php include 'includes/header.php';
 
+//Incluir las otras clases
+require 'clases/Clientes.php';
+require 'clases/Detalles.php';
+
+function MiAutoload($clase)
+{
+    require __DIR__.'/clases/'.$clase.'.php';
+}
+
+spl_autoload_register('MiAutoload');
+
+$detalles = new Detalles();
+$clienter = new Clientes();
 
 include 'includes/footer.php';
