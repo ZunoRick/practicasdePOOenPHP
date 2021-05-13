59ed70808af50746166796d14cca5139be1d3b48
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-13 02:20:03 -0500

    Modifique archivo 10- Consulta de BD con PDO, POO y prepare statements

--- 10.php
+++ 10.php
@@ -1,4 +1,33 @@
 <?php include 'includes/header.php';
 
+$username = 'root';
+$passwd = 'root';   
+$dbname = 'bienes_raices';
+$host = 'localhost';
+$dbname = 'bienes_raices';
+$port = 3310;
+
+//Conectar a la BD con PDO
+$db = new PDO("mysql:host=${host};dbname=${dbname};charset=UTF8;port=${port}", $username, $passwd);
+
+//Creamos el query
+$query = "SELECT titulo, imagen FROM propiedades";
+
+//Lopreparamos
+$stmt = $db->prepare($query);
+
+//Lo ejecutamos
+$stmt->execute();
+
+//Obtener los resultados
+$resultado = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+//Iterar
+foreach ($resultado as $propiedad):
+    echo $propiedad['titulo'];
+    echo "<br>";
+    echo $propiedad['imagen'];
+    echo "<br>";
+endforeach;
 
 include 'includes/footer.php';
