50d6fae71e08eab3e2abd84d0cc96e899393a54c
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-12 22:47:29 -0500

    Modificando el archivo 08 usando composer

--- 08.php
+++ 08.php
@@ -1,4 +1,6 @@
-<?php include 'includes/header.php';
+<?php 
+    include 'includes/header.php';
+    include 'vendor/autoload.php';
 
 //Incluir las otras clases
 // require 'clases/Clientes.php';
@@ -7,13 +9,13 @@
 use App\Clientes;
 use App\Detalles;
 
-function MiAutoload($clase)
-{   
-    $partes = explode('\\', $clase);
-    require __DIR__.'/clases/'. $partes[1] .'.php';
-}
-
-spl_autoload_register('MiAutoload');
+//Añadiendo clases con autoload sin composer 
+// function MiAutoload($clase)
+// {   
+//     $partes = explode('\\', $clase);
+//     require __DIR__.'/clases/'. $partes[1] .'.php';
+// }
+//spl_autoload_register('MiAutoload');
 
 $detalles = new Detalles();
 $clienter = new Clientes();
